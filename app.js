1ce39dccbb370b6f1b5e7d663351b642624f6414
mateuszmagier <35307532+mateuszmagier@users.noreply.github.com>
2021-11-16 15:08:17 +0100

    change port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ router.get('/',function(req,res){
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running server');
